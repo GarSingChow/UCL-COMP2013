1e7887f093df0314f7a7cf2213833bad267b28b5
GarSing Chow <GarSingChow@googlemail.com>
2014-10-24 18:59:04 +0100

    add search.php

---
+++ search.php
@@ -0,0 +1,81 @@
+<html>
+<head>
+<Title>Search Form</Title>
+<style type="text/css">
+    body { background-color: #fff; border-top: solid 10px #000;
+        color: #333; font-size: .85em; margin: 20; padding: 20;
+        font-family: "Segoe UI", Verdana, Helvetica, Sans-Serif;
+    }
+    h1, h2, h3,{ color: #000; margin-bottom: 0; padding-bottom: 0; }
+    h1 { font-size: 2em; }
+    h2 { font-size: 1.75em; }
+    h3 { font-size: 1.2em; }
+    table { margin-top: 0.75em; }
+    th { font-size: 1.2em; text-align: left; border: none; padding-left: 0; }
+    td { padding: 0.25em 2em 0.25em 0em; border: 0 none; }
+</style>
+</head>
+<body>
+<h1>Search the database here!</h1>
+    <p>Fill in a name, then click <strong>Submit</strong> to search.</p>
+<form method="post" action="index.php" enctype="multipart/form-data" >
+      Name  <input type="text" name="name" id="name"/></br>
+      <input type="submit" name="submit" value="Submit" />
+</form>
+<?php
+    // DB connection info
+    $host = "eu-cdbr-azure-west-b.cloudapp.net";
+    $user = "ba43e3a9c0b64e";
+    $pwd = "ebc318aa";
+    $db = "UCLCOMPAxCfqTIWt";
+    // Connect to database.
+    try {
+        $conn = new PDO( "mysql:host=$host;dbname=$db", $user, $pwd);
+        $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+    }
+    catch(Exception $e){
+        die(var_dump($e));
+    }
+    // Insert registration info
+    if(!empty($_POST)) {
+    try {
+        $name = $_POST['name'];
+        // Insert data
+        $sql_select = "SELECT * FROM registration_tbl WHERE name LIKE '%$name%'";
+	$stmt = $conn->query($sql_select);
+	$registrants = $stmt->fetchAll(); 
+	if(count($registrants) > 0) {
+	  echo "<h2>People that match your search:</h2>";
+	  echo "<table>";
+	  echo "<tr><th>Name</th>";
+	  echo "<th>Email</th>";
+	  echo "<th>Date</th>";
+	  echo "<th>Company Name</th></tr>";
+	  foreach($registrants as $registrant) {
+            echo "<tr><td>".$registrant['name']."</td>";
+            echo "<td>".$registrant['email']."</td>";
+            echo "<td>".$registrant['date']."</td>";
+	    echo "<td>".$registrant['company']."</td></tr>";
+	  }
+	  echo "</table>";
+	} else {
+	  echo "<h3>No search results.</h3>";
+	}
+	/*
+	$sql_search = "INSERT INTO registration_tbl (name, email, date, company)
+                   VALUES (?,?,?,?)";
+        $stmt = $conn->prepare($sql_insert);
+        $stmt->bindValue(1, $name);
+        $stmt->bindValue(2, $email);
+        $stmt->bindValue(3, $date);
+	$stmt->bindValue(4, $company);
+        $stmt->execute();*/
+    }
+    catch(Exception $e) {
+        die(var_dump($e));
+    }
+    echo "<h3>Searching...!</h3>";
+    }
+?>
+</body>
+</html>
